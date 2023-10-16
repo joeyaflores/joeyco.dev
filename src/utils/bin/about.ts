e40a43d2f935fb6245a36127215e3afb569d9cd1
Joey Flores <joey.flores@tcu.edu>
2023-10-15 21:54:18 -0500

    redeploy after adding env vars in vercel

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -3,7 +3,7 @@ import { getBio } from '../../api';
 export const about = async (args: string[]): Promise<string> => {
   let bio = await getBio();
   bio += `
-Use the 'linkedin' or 'email' commands to contact me.
+Use the 'linkedin' or 'email' commands to contact me.   
   `
 
   return bio;

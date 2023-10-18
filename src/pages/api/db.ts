31c2eca75742b279542c2d37a434607fac71c113
Joey Flores <joey.flores@tcu.edu>
2023-10-18 00:40:57 -0500

    comment for now

--- src/pages/api/db.ts
+++ src/pages/api/db.ts
@@ -1,21 +1,17 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import client from '../../../utils/mongo-client';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import {getUser} from '../../../utils/searchUser';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    await client.connect();
-    const db_name = process.env.MONGODB_DB
-    const collection = client.db(db_name).collection("feedback");
-    
-    const data = await collection.find({}).toArray();
-    
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "An error occurred." });
-  } finally {
-    await client.close();
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const result = await getUser();
+//     return res.status(200).json(result);
+//   } catch (e: any) {
+//     return res.status(500).json({
+//       error: e.toString()
+//     });
+//   }
+// }
+

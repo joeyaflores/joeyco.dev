e00b5ee39d1d4ad7bc60eb08dbdab08354a868e9
Joey Flores <joey.flores@tcu.edu>
2023-10-18 00:49:10 -0500

    remove tab completion for now

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -2,7 +2,7 @@ import { useMatomo } from '@m4tt72/matomo-tracker-react';
 import React, { useEffect, useState } from 'react';
 import { commandExists } from '../../utils/commandExists';
 import { useShell } from '../../utils/shellProvider';
-import { handleTabCompletion } from '../../utils/tabCompletion';
+// import { handleTabCompletion } from '../../utils/tabCompletion';
 import { useTheme } from '../../utils/themeProvider';
 import { Ps1 } from '../ps1';
 
@@ -44,11 +44,11 @@ export const Input = ({ inputRef, containerRef }) => {
       clearHistory();
     }
 
-    if (event.key === 'Tab') {
-      event.preventDefault();
+    // if (event.key === 'Tab') {
+    //   event.preventDefault();
 
-      handleTabCompletion(value, setValue);
-    }
+    //   handleTabCompletion(value, setValue);
+    // }
 
     if (event.key === 'Enter' || event.code === '13') {
       event.preventDefault();
